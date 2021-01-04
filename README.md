c122818c77ee0134f9a081045a3ed4b0613d8d4e
Hardefa <hardefarogonondo@gmail.com>
2021-01-04 19:44:51 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Value Comparator
+This is one of the task of Pirple Python is Easy online course.
+
+The task is to create a function that accepts 3 parameters and checks for equality between any two of them. The function should return True if 2 or more of the parameters are equal, and false is none of them are equal to any of the others.
+
+Modify the function so that strings can be compared to integers if they are equivalent. For example, if the following values are passed to your function:
+```
+6,5,"5"
+```
+It should returns true instead of false.
---
+++ main.py
@@ -0,0 +1,28 @@
+# Initialization
+Indicator = False
+
+# Functions
+def Compare(xVal, yVal, zVal):
+    global Indicator
+    if xVal == yVal or xVal == zVal or yVal == zVal:
+        Indicator = True
+    else:
+        Indicator = False
+
+def indicator():
+    if Indicator == True:
+        print("Yes! There is a same value!")
+    else:
+        print("No! They are all different value!")
+
+# Main Program
+print("Let's compare the values!")
+print("Please insert any value to the following variables")
+xVal = int(input("X = "))
+yVal = int(input("Y = "))
+zVal = int(input("Z = "))
+
+Start = input("Whether any of the value are the same? Press Enter to compare!")
+if Start == "":
+    Compare(xVal, yVal, zVal)
+    indicator()
